0d162ace7c7915b7a3a732489e085c5c5d083d31
ksrajth7 <rajathkizhiyedam@gmail.com>
2020-08-11 20:09:01 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Header/>
         
         <Switch>
-          <Route exact path="/" render={
+          <Route exact path={`${process.env.PUBLIC_URL}/`} render={
             ()=><div className="app__page">
               <SidebarCollapsable subscriptions={false} home={true} trending={false} />
               <Sidebar  subscriptions={false} home={true} trending={false} />
@@ -25,14 +25,14 @@ function App() {
           </div>}>
             
           </Route>
-          <Route path="/search/:searchTerm" render={
+          <Route path={`${process.env.PUBLIC_URL}/search/:searchTerm`} render={
             (props)=><div className="app__page">
               <SidebarCollapsable />
               <Sidebar/>
               <SearchPage a={props.match.params.searchTerm}/></div>} >
           
           </Route>
-          <Route path="/Subscriptions" render={
+          <Route path={`${process.env.PUBLIC_URL}/Subscriptions`} render={
             ()=><div className="app__page">
               <SidebarCollapsable subscriptions={true} home={false} trending={false}  />
               <Sidebar subscriptions={true} home={false} trending={false} />
@@ -40,7 +40,7 @@ function App() {
               </div>} >
           
           </Route>
-          <Route path="/Home" render={
+          <Route path={`${process.env.PUBLIC_URL}/Home`} render={
             ()=><div className="app__page">
               <SidebarCollapsable subscriptions={false} home={true} trending={false} />
               <Sidebar subscriptions={false} home={true} trending={false}/>
@@ -48,7 +48,7 @@ function App() {
               </div>} >
           
           </Route>
-          <Route path="/Trending" render={
+          <Route path={`${process.env.PUBLIC_URL}/Trending`} render={
             ()=><div className="app__page">
               <SidebarCollapsable subscriptions={false} home={false} trending={true} />
               <Sidebar subscriptions={false} home={false} trending={true} />
