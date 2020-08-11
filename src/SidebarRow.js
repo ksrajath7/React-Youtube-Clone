c0e4fdcf1b9c818e0a1e1d0e3511f2c855996000
ksrajth7 <rajathkizhiyedam@gmail.com>
2020-08-11 20:32:35 +0530

    hello

--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 import './SidebarRow.css'
 function SidebarRow({title, Icon, selected}) {
     return (
-        <NavLink to={`/${title}`} style={{textDecoration:'none'}}>
+        <NavLink to={`${process.env.PUBLIC_URL}/${title}`} style={{textDecoration:'none'}}>
             <div className={`sidebarRow ${selected && "selected"}`}>
                 <Icon className="sidebarRow__icon"/>
                 <h2 className="sidebarRow__title">{title}</h2>
